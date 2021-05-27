ce674829b42b01e827b77a8ff5e487ea7518e81e
Thanuja Mattam <mattamthanujacse.gist@gmail.com>
2021-05-27 22:18:40 +0530

    Evenood.java, HelloWorld.java, Multiplication.java are added

---
+++ Evenodd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Evenodd
+{
+public static void main(String args[])
+{
+int a;
+Scanner s = new Scanner(System.in);
+System.out.print("Enter a number : ");
+a=s.nextInt();
+if(a%2==0)
+System.out.println(a+" is even number");
+else
+System.out.println(a+" is odd number");
+}
+}
+
